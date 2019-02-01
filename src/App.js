15af28bc058d852493bddb9dec3efdd302561635
katia1802 <katia.rojas.sandoval@gmail.com>
2019-02-01 19:12:19 +0100

    Getting API call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let repositoryName = 'aui';
+
+let repositoryId = '';
+
+const apiCall = `https://api.bitbucket.org/2.0/repositories/atlassian/${repositoryName}/pullrequests/${repositoryId}`
+
+
+
 class App extends Component {
+  constructor (props){
+    super (props);
+    this.state = {
+    pullRequest: [],
+    }
+    console.log (this.state.title)
+  }
+  
+  componentDidMount (){
+    fetch (apiCall)
+      .then (response => response.json())
+      .then (data => {
+        console.log (data.values)
+        const pullRequestInfo = data.values.map((item, index)=>{
+        
+          return {
+            state: item.state,
+            date: item.created_on,
+            title: item.title,
+            author: item.author.display_name,
+            comments: item.comment_count,
+          }
+        }
+        
+        
+        
+        )
+        // console.log (pullRequestInfo)
+        this.setState ({
+          // title: pullRequestInfo.title,
+          pullRequest: pullRequestInfo
+          // state: pullRequestInfo.state,
+        }
+        // console.log (this.state.title)
+        )
+
+      })
+  }
+
   render() {
     return (
       <div className="App">
+
+
 
       </div>
     );
