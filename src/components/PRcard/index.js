afd969665138c36cad756157b36d79f7af3e5d7f
elisamartinb <elisamartinbt@gmail.com>
2019-02-02 22:53:09 +0100

    styles in pr title

--- src/components/PRcard/index.js
+++ src/components/PRcard/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import User from '../User';
-import Reasons from '../Reasons';
-import './PRcard.scss';
+import React, { Component } from "react";
+import User from "../User";
+import Reasons from "../Reasons";
+import "./PRcard.scss";
 
 class PRcard extends Component {
   render() {
@@ -9,12 +9,14 @@ class PRcard extends Component {
       <div className="prcard__container">
         <div className="dateandcomments__container">
           <span className="prcard__date">22/01/2019</span>
-          <span className="prcard__numcomments">4<i className="far fa-comment"></i></span>
+          <span className="prcard__numcomments">
+            4<i className="far fa-comment" />
+          </span>
         </div>
         <h3 className="prcard__title">PR title</h3>
-        <i className="fas fa-arrow-right"></i>
         <div className="users__container">
           <User />
+          <i className="fas fa-arrow-right" />
           <User />
         </div>
         <Reasons />
