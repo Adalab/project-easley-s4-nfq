7141632315c1d508fcaef1c60efcb27d1e64b7ed
elisamartinb <elisamartinbt@gmail.com>
2019-02-02 21:55:10 +0100

    add PRcard in PRlist

--- src/components/PRcard/index.js
+++ src/components/PRcard/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import User from '../User';
 import Reasons from '../Reasons';
+import './PRcard.scss';
 
 class PRcard extends Component {
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
                 <span>22/01/2019</span>
-                <span>4<i class="far fa-comment"></i></span>
+                <span>4<i className="far fa-comment"></i></span>
                 <h3>PR title</h3>
-                <i class="fas fa-arrow-right"></i>
+                <i className="fas fa-arrow-right"></i>
                 <User />
                 <User />
                 <Reasons />
