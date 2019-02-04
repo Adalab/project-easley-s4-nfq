1a22f0f5a9fb65de476e3a211e539c1a696e453c
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-02-04 11:13:31 +0100

    add services

--- src/App.js
+++ src/App.js
@@ -5,27 +5,20 @@ import Header from './components/Header';
 import Summary from './components/Summary';
 import Footer from './components/Footer';
 import DetailsContainer from './components/DetailsContainer';
-
-let repositoryName = 'aui';
-
-let repositoryId = '';
-
-const apiCall = `https://api.bitbucket.org/2.0/repositories/atlassian/${repositoryName}/pullrequests/${repositoryId}`
-
+import getPullRequestInfo from './Services/RepositoryService';
 
 class App extends Component {
-  constructor (props){
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-    pullRequests: [],
+      pullRequests: [],
     }
   }
 
-  componentDidMount (){
-    fetch (apiCall)
-      .then (response => response.json())
-      .then (data => {
-        const pullRequestInfo = data.values.map((item, index)=>{
+  componentDidMount() {
+    getPullRequestInfo()
+      .then(data => {
+        const pullRequestInfo = data.values.map((item, index) => {
           return {
             state: item.state,
             date: item.created_on,
@@ -37,50 +30,51 @@ class App extends Component {
           }
         })
 
-        this.setState ({
+        this.setState({
           pullRequests: pullRequestInfo
         })
       })
   }
 
   render() {
-    const {pullRequests} = this.state;
+    const { pullRequests } = this.state;
 
     return (
       <div className="App">
-      {pullRequests.map((item, index) => {
-      return (
-        <div key={index}>
-         <h3>{item.title}</h3>
-         <h4>{item.date}</h4>
-         <h4>{item.comments}</h4>
-         <img src={item.avatar} alt={item.author} />
-         <h4>{item.author}</h4>
-         <h4>{item.branch}</h4>
-        </div>
-      )})}
+        {pullRequests.map((item, index) => {
+          return (
+            <div key={index}>
+              <h3>{item.title}</h3>
+              <h4>{item.date}</h4>
+              <h4>{item.comments}</h4>
+              <img src={item.avatar} alt={item.author} />
+              <h4>{item.author}</h4>
+              <h4>{item.branch}</h4>
+            </div>
+          )
+        })}
         <Header />
         <main>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return (
-                <Summary />
-              )
-            }}
-          />
-          <Route
-            exact
-            path="/details"
-            render={() => {
-              return (
-                <DetailsContainer />
-              )
-            }}
-          />
-        </Switch>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return (
+                  <Summary />
+                )
+              }}
+            />
+            <Route
+              exact
+              path="/details"
+              render={() => {
+                return (
+                  <DetailsContainer />
+                )
+              }}
+            />
+          </Switch>
         </main>
         <Footer />
       </div>
