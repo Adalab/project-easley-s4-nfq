84d8723ad0b0a84df9f7ea16acef0c4a53aa178e
elisamartinb <elisamartinbt@gmail.com>
2019-02-04 11:48:15 +0100

    edit date from API in a new format

--- src/App.js
+++ src/App.js
@@ -14,18 +14,18 @@ const apiCall = `https://api.bitbucket.org/2.0/repositories/atlassian/${reposito
 
 
 class App extends Component {
-  constructor (props){
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-    pullRequests: [],
+      pullRequests: [],
     }
   }
 
-  componentDidMount (){
-    fetch (apiCall)
-      .then (response => response.json())
-      .then (data => {
-        const pullRequestInfo = data.values.map((item, index)=>{
+  componentDidMount() {
+    fetch(apiCall)
+      .then(response => response.json())
+      .then(data => {
+        const pullRequestInfo = data.values.map((item, index) => {
           return {
             state: item.state,
             date: item.created_on,
@@ -37,50 +37,70 @@ class App extends Component {
           }
         })
 
-        this.setState ({
+        this.setState({
           pullRequests: pullRequestInfo
         })
       })
   }
 
+  handleDate(date) {
+    let newDate = date.substring(0, 10);
+    newDate = newDate.split("-");
+    newDate = newDate.reverse();
+    const dayDate = parseInt(newDate[0]);
+    const monthDate = parseInt(newDate[1]);
+    const yearDate = parseInt(newDate[2]);
+    newDate = newDate.join("-");
+    const infoDate = {
+      date: newDate,
+      day: dayDate,
+      month: monthDate,
+      year: yearDate,
+    }
+    return infoDate
+  }
+
   render() {
-    const {pullRequests} = this.state;
+    const { pullRequests } = this.state;
 
     return (
       <div className="App">
-      {pullRequests.map((item, index) => {
-      return (
-        <div key={index}>
-         <h3>{item.title}</h3>
-         <h4>{item.date}</h4>
-         <h4>{item.comments}</h4>
-         <img src={item.avatar} alt={item.author} />
-         <h4>{item.author}</h4>
-         <h4>{item.branch}</h4>
-        </div>
-      )})}
+        {pullRequests.map((item, index) => {
+          const newFormatedDate = this.handleDate(item.date);
+          console.log(newFormatedDate)
+          return (
+            <div key={index}>
+              <h3>{item.title}</h3>
+              <h4>{newFormatedDate.date}</h4>
+              <h4>{item.comments}</h4>
+              <img src={item.avatar} alt={item.author} />
+              <h4>{item.author}</h4>
+              <h4>{item.branch}</h4>
+            </div>
+          )
+        })}
         <Header />
         <main>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return (
-                <Summary />
-              )
-            }}
-          />
-          <Route
-            exact
-            path="/details"
-            render={() => {
-              return (
-                <DetailsContainer />
-              )
-            }}
-          />
-        </Switch>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return (
+                  <Summary />
+                )
+              }}
+            />
+            <Route
+              exact
+              path="/details"
+              render={() => {
+                return (
+                  <DetailsContainer />
+                )
+              }}
+            />
+          </Switch>
         </main>
         <Footer />
       </div>
