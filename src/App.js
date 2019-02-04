be1ca7189e1d8f6efabd4ca395b64aedd2383c13
katia1802 <katia.rojas.sandoval@gmail.com>
2019-02-04 10:57:57 +0100

    Layout footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ let repositoryName = 'aui';
 
 let repositoryId = '';
 
-const apiCall = `https://api.bitbucket.org/2.0/repositories/atlassian/${repositoryName}/pullrequests/${repositoryId}`
+const uri = `https://api.bitbucket.org/2.0/repositories/atlassian/${repositoryName}/pullrequests/${repositoryId}`
 
 
 class App extends Component {
@@ -21,9 +21,10 @@ class App extends Component {
   }
 
   componentDidMount (){
-    fetch (apiCall)
+    fetch (uri)
       .then (response => response.json())
       .then (data => {
+        console.log (data)
         const pullRequestInfo = data.values.map((item, index)=>{
           return {
             state: item.state,
@@ -50,12 +51,14 @@ class App extends Component {
       {pullRequests.map((item, index) => {
       return (
         <div key={index}>
-         <h3>{item.title}</h3>
-         <h4>{item.date}</h4>
-         <h4>{item.comments}</h4>
-         <img src={item.avatar} alt={item.author} />
-         <h4>{item.author}</h4>
-         <h4>{item.branch}</h4>
+         <h3 className="app--card-title">{item.title}</h3>
+         <h4 className="app--card-date">{item.date}</h4>
+         <h4 className="app--card-comments">{item.comments}</h4>
+         <div className="app--card-user">
+         <img className="app--card-image" src={item.avatar} alt={item.author} />
+         <h4 className="app--card-name">{item.author}</h4>
+         <h4 className="app--card-branch">{item.branch}</h4>
+         </div>
         </div>
       )})}
         <Header />
