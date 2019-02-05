672a5296577d6ae5e952f94895901b4d8da77eb8
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-02-05 16:59:57 +0100

    add repository select

--- src/App.js
+++ src/App.js
@@ -19,16 +19,17 @@ class App extends Component {
   }
 
   changeRepository(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
-  componentDidUpdate(prevState) {
-    if (this.state.value !== prevState.value) {
+  componentDidMount() {
     this.getRepository();
-    }
   }
-  componentDidMount(){
-    this.getRepository();
+
+  componentDidUpdate(prepProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      this.getRepository();
+    }
   }
 
   getRepository() {
@@ -40,47 +41,47 @@ class App extends Component {
     console.log(prEndpoint);
 
     fetch(prEndpoint)
-    .then(response => response.json())
-    .then(data => {
-      const pullRequestInfo = data.values.map((item, index) => {
-        return {
-          id: item.id,
-          state: item.state,
-          date: item.created_on,
-          title: item.title,
-          author: item.author.display_name,
-          comments_number: item.comment_count,
-          avatar: item.author.links.avatar.href,
-          branch: item.source.branch.name,
-          develop: item.destination.branch.name,
-          uriReviewer: prEndpoint + item.id,
-          repository: item.destination.repository.full_name,
-        };
-      });
-      this.setState({
-        pullRequests: pullRequestInfo
-      });
-
-      const uriReviewer = this.state.pullRequests[0].uriReviewer;
-      console.log("uriReviewer", uriReviewer);
-      console.log("state pullrequests", this.state.pullRequests);
-
-      fetch(uriReviewer)
-        .then(response => response.json())
-        .then(data => {
-          console.log("data", data);
-          const pullRequestReviewer = data.reviewers.map((item, index) => {
-            return {
-              reviewer_name: item.display_name,
-              reviewer_avatar: item.links.avatar.href
-            };
-          });
-          this.setState({
-            reviewers: pullRequestReviewer
-          });
-          console.log(this.state.reviewers);
+      .then(response => response.json())
+      .then(data => {
+        const pullRequestInfo = data.values.map((item, index) => {
+          return {
+            id: item.id,
+            state: item.state,
+            date: item.created_on,
+            title: item.title,
+            author: item.author.display_name,
+            comments_number: item.comment_count,
+            avatar: item.author.links.avatar.href,
+            branch: item.source.branch.name,
+            develop: item.destination.branch.name,
+            uriReviewer: prEndpoint + item.id,
+            repository: item.destination.repository.full_name,
+          };
         });
-    });
+        this.setState({
+          pullRequests: pullRequestInfo
+        });
+
+        const uriReviewer = this.state.pullRequests[0].uriReviewer;
+        console.log("uriReviewer", uriReviewer);
+        console.log("state pullrequests", this.state.pullRequests);
+
+        fetch(uriReviewer)
+          .then(response => response.json())
+          .then(data => {
+            console.log("data", data);
+            const pullRequestReviewer = data.reviewers.map((item, index) => {
+              return {
+                reviewer_name: item.display_name,
+                reviewer_avatar: item.links.avatar.href
+              };
+            });
+            this.setState({
+              reviewers: pullRequestReviewer
+            });
+            console.log(this.state.reviewers);
+          });
+      });
   }
 
   handleDate(date) {
