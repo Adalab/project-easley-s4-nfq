e7a4060d72a41c4d448d0ba2aa5d7a3d11222986
clara <clarisaharguindey@gmail.com>
2019-02-05 19:18:27 +0100

    publish master

---
+++ docs/precache-manifest.cdce7d6874c90e96c5bfa11302a4501c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cfc3da0079f63eb2d8f2",
+    "url": "./static/js/main.cfc3da00.chunk.js"
+  },
+  {
+    "revision": "de369c24e5b6595f39e1",
+    "url": "./static/js/1.de369c24.chunk.js"
+  },
+  {
+    "revision": "cfc3da0079f63eb2d8f2",
+    "url": "./static/css/main.fae3afb6.chunk.css"
+  },
+  {
+    "revision": "28a8eea8dad7020038f63cdffb76adeb",
+    "url": "./index.html"
+  }
+];
