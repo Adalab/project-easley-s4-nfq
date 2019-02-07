1d4e9c0d1262ed2ee7a60fc22e1368b680833f8c
katia1802 <katia.rojas.sandoval@gmail.com>
2019-02-07 20:40:20 +0100

    Image number of reviewers

--- src/components/Reviewers/index.js
+++ src/components/Reviewers/index.js
@@ -1,17 +1,38 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
 class Reviewers extends Component {
   render() {
-    const {rvAvatar, rvName, develop} = this.props
-    return (
-      <Fragment>
-        <div>
-          <img src={rvAvatar} alt={rvName}/>
-          <h4>{rvName}</h4>
-          <h4>{develop}</h4>
-        </div>
-      </Fragment>
-     );
+    const { reviewers } = this.props;
+    console.log("develop", this.props.develop);
+    if (reviewers.length === 1) {
+      return (
+        <Fragment>
+          {reviewers.map((rv, index) => {
+            return (
+              <div key={index}>
+                <img src={rv.links.avatar.href} alt={rv.display_name} />
+                <h4>{rv.display_name}</h4>
+                <h4>{this.props.develop}</h4>
+              </div>
+            );
+          })}
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          <img src="https://image.flaticon.com/icons/svg/9/9463.svg" />
+          {reviewers.map((rv, index) => {
+            return (
+              <div key={index}>
+                <h4>{rv.display_name}</h4>
+              </div>
+            );
+          })}
+          <h4>{this.props.develop}</h4>
+        </Fragment>
+      );
+    }
   }
 }
 
