a94854eae79e2f36640e49bd1daf3a228d96e43b
clara <clarisaharguindey@gmail.com>
2019-02-18 10:18:41 +0100

    update branch

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -8,10 +8,14 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
+  PieChart,
+  Pie,
+  Cell
 } from "recharts";
 
 class Summary extends Component {
+
   componentDidMount() {
     this.props.getRepository(null, "OPEN", "summary");
     this.props.getRepository(null, "MERGED", "summary");
@@ -19,21 +23,21 @@ class Summary extends Component {
     this.props.getToken();
   }
 
-  getDataforChart(status){
-    let data="";
-    if(status === "open"){
+  getDataforChart(status) {
+    let data = "";
+    if (status === "open") {
       data = this.props.summaryData.totalOpen.flat()
     }
-    else if(status === "declined"){
+    else if (status === "merged") {
       data = this.props.summaryData.totalMerged.flat()
     }
-    else if(status === "merged"){
+    else if (status === "declined") {
       data = this.props.summaryData.totalDeclined.flat()
     }
 
     const namesData = data.map(user =>
-     user.author ?
-     user.author.display_name : ""
+      user.author ?
+        user.author.display_name : ""
     )
 
     const countedName = countBy(namesData);
@@ -45,15 +49,27 @@ class Summary extends Component {
         name: key,
         value: value
       }
-  });
+    });
     return newobject
   }
 
+  renderCustomizedLabel({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * Math.PI / 180);
+    const y = cy + radius * Math.sin(-midAngle * Math.PI / 180);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
   render() {
     const openForChart = this.getDataforChart("open")
     const mergedForChart = this.getDataforChart("merged")
     const declinedForChart = this.getDataforChart("declined")
 
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#00e8fe', '#c40003', '#deff28', '#ff6441'];
     return (
       <div className="summary-firstGraph">
         <BarChart
@@ -81,13 +97,64 @@ class Summary extends Component {
           <Tooltip />
           <Legend />
           <Bar
-          minPointSize={10} label={{ fill: '#979797', fontSize: 20, position: "right" }}
-          stroke="#8884d8"
-          isAnimationActive={true}
-          animationDuration={500}
-          barSize={50}
-          dataKey="qty" fill="#82ca9d" />
+            minPointSize={10} label={{ fill: '#979797', fontSize: 20, position: "right" }}
+            stroke="#8884d8"
+            isAnimationActive={true}
+            animationDuration={500}
+            barSize={50}
+            dataKey="qty" fill="#82ca9d" />
         </BarChart>
+
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={openForChart}
+            cx={300}
+            cy={200}
+            labelLine={false}
+            label={this.renderCustomizedLabel(300, 200)}
+            outerRadius={190}
+            fill="#8884d8"
+          >
+
+            {
+              openForChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+            }
+          </Pie>
+        </PieChart>
+
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={mergedForChart}
+            cx={300}
+            cy={200}
+            labelLine={false}
+            label={this.renderCustomizedLabel(300, 200)}
+            outerRadius={190}
+            fill="#8884d8"
+          >
+
+            {
+              mergedForChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+            }
+          </Pie>
+        </PieChart>
+
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={declinedForChart}
+            cx={300}
+            cy={200}
+            labelLine={true}
+            label={this.renderCustomizedLabel(300, 200)}
+            outerRadius={190}
+            fill="#8884d8"
+          >
+
+            {
+              declinedForChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+            }
+          </Pie>
+        </PieChart>
       </div>
     );
   }
